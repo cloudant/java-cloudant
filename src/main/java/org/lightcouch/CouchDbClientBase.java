b75cd4269b5beeb2a7bb5f99de98c01de19d59d3
Ahmed Yehia <ahmed.yehia.m@gmail.com>
2011-09-30 03:44:29 +0200

    unit test fix, license txt

--- src/main/java/org/lightcouch/CouchDbClientBase.java
+++ src/main/java/org/lightcouch/CouchDbClientBase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Ahmed Yehia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lightcouch;
 
 import static java.lang.String.format;
@@ -65,7 +81,7 @@ import com.google.gson.JsonParser;
  * @see CouchDbClient
  * @author Ahmed Yehia
  */
-public abstract class CouchDbClientBase {
+abstract class CouchDbClientBase {
 
 	private static final Log log = LogFactory.getLog(CouchDbClientBase.class);
 	
@@ -92,7 +108,10 @@ public abstract class CouchDbClientBase {
 	
 	// ---------------------------------------------- Getters
 	
-	public URI getDBUri() {
+	/**
+	 * @return The database URI.
+	 */
+	protected URI getDBUri() {
 		return dbURI;
 	}
 	
@@ -357,7 +376,7 @@ public abstract class CouchDbClientBase {
 	/**
 	 * Shuts down the connection manager used by this client instance.
 	 */
-	public void shutdown() {
+	protected void shutdown() {
 		this.httpClient.getConnectionManager().shutdown();
 	}
 }
--- src/test/java/org/lightcouch/tests/CouchDbClientTest.java
+++ src/test/java/org/lightcouch/tests/CouchDbClientTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Ahmed Yehia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lightcouch.tests;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -333,7 +349,7 @@ public class CouchDbClientTest {
 		System.out.println("------------------------------- Replicate Conflict Test Started");
     	
 		DesignDocument conflictsDoc = dbClient.design().getFromDesk("conflicts");
-		dbClient.design().synchronizeWithDb(conflictsDoc);
+		dbClient2.design().synchronizeWithDb(conflictsDoc);
     	
 		String anId = UUID.randomUUID().toString();
 		Foo foodb1 = new Foo(anId, "title", 0);
