1d49e04e462317fe31f3b3ff0dc4187ed09f92d7
dvberkel <daan.vanberkel@nedap.com>
2013-05-15 14:44:52 +0200

    introduce a fluent interface for CouchDbProperties
    fixes issue #24

--- src/main/java/org/lightcouch/CouchDbProperties.java
+++ src/main/java/org/lightcouch/CouchDbProperties.java
@@ -18,7 +18,7 @@ package org.lightcouch;
 
 /**
  * Represents configuration properties for connecting to CouchDB.
- * 
+ *
  * @author Ahmed Yehia
  */
 public class CouchDbProperties {
@@ -32,7 +32,7 @@ public class CouchDbProperties {
 	private String username;
 	private String password;
 
-	// optional 
+	// optional
 	private int socketTimeout;
 	private int connectionTimeout;
 	private int maxConnections;
@@ -94,7 +94,7 @@ public class CouchDbProperties {
 	public int getMaxConnections() {
 		return maxConnections;
 	}
-	
+
 	public String getProxyHost() {
 		return proxyHost;
 	}
@@ -103,56 +103,147 @@ public class CouchDbProperties {
 		return proxyPort;
 	}
 
-	public void setDbName(String dbName) {
+	public CouchDbProperties setDbName(String dbName) {
 		this.dbName = dbName;
+		return this;
 	}
 
-	public void setCreateDbIfNotExist(boolean createDbIfNotExist) {
+	public CouchDbProperties setCreateDbIfNotExist(boolean createDbIfNotExist) {
 		this.createDbIfNotExist = createDbIfNotExist;
+		return this;
 	}
 
-	public void setProtocol(String protocol) {
+	public CouchDbProperties setProtocol(String protocol) {
 		this.protocol = protocol;
+		return this;
 	}
 
-	public void setHost(String host) {
+	public CouchDbProperties setHost(String host) {
 		this.host = host;
+		return this;
 	}
 
-	public void setPort(int port) {
+	public CouchDbProperties setPort(int port) {
 		this.port = port;
+		return this;
 	}
 
-	public void setUsername(String username) {
+	public CouchDbProperties setUsername(String username) {
 		this.username = username;
+		return this;
 	}
 
-	public void setPassword(String password) {
+	public CouchDbProperties setPassword(String password) {
 		this.password = password;
+		return this;
 	}
 
-	public void setSocketTimeout(int socketTimeout) {
+	public CouchDbProperties setSocketTimeout(int socketTimeout) {
 		this.socketTimeout = socketTimeout;
+		return this;
 	}
 
-	public void setConnectionTimeout(int connectionTimeout) {
+	public CouchDbProperties setConnectionTimeout(int connectionTimeout) {
 		this.connectionTimeout = connectionTimeout;
+		return this;
 	}
 
-	public void setMaxConnections(int maxConnections) {
+	public CouchDbProperties setMaxConnections(int maxConnections) {
 		this.maxConnections = maxConnections;
-	}
-	
-	public void setProxyHost(String proxyHost) {
-		this.proxyHost = proxyHost;
+		return this;
 	}
 
-	public void setProxyPort(int proxyPort) {
+	public CouchDbProperties setProxyHost(String proxyHost) {
+		this.proxyHost = proxyHost;
+		return this;
+	}
+
+	public CouchDbProperties setProxyPort(int proxyPort) {
 		this.proxyPort = proxyPort;
+		return this;
 	}
 
 	public void clearPassword() {
 		setPassword("");
 		setPassword(null);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + connectionTimeout;
+		result = prime * result + (createDbIfNotExist ? 1231 : 1237);
+		result = prime * result + ((dbName == null) ? 0 : dbName.hashCode());
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + maxConnections;
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + port;
+		result = prime * result
+				+ ((protocol == null) ? 0 : protocol.hashCode());
+		result = prime * result
+				+ ((proxyHost == null) ? 0 : proxyHost.hashCode());
+		result = prime * result + proxyPort;
+		result = prime * result + socketTimeout;
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CouchDbProperties other = (CouchDbProperties) obj;
+		if (connectionTimeout != other.connectionTimeout)
+			return false;
+		if (createDbIfNotExist != other.createDbIfNotExist)
+			return false;
+		if (dbName == null) {
+			if (other.dbName != null)
+				return false;
+		} else if (!dbName.equals(other.dbName))
+			return false;
+		if (host == null) {
+			if (other.host != null)
+				return false;
+		} else if (!host.equals(other.host))
+			return false;
+		if (maxConnections != other.maxConnections)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (port != other.port)
+			return false;
+		if (protocol == null) {
+			if (other.protocol != null)
+				return false;
+		} else if (!protocol.equals(other.protocol))
+			return false;
+		if (proxyHost == null) {
+			if (other.proxyHost != null)
+				return false;
+		} else if (!proxyHost.equals(other.proxyHost))
+			return false;
+		if (proxyPort != other.proxyPort)
+			return false;
+		if (socketTimeout != other.socketTimeout)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+
 }
