d936f55b5d6c44aef6dd6265302f91855f65c9a7
Rich Ellis <ricellis@users.noreply.github.com>
2015-09-23 17:49:34 +0100

    Added client and db ExternalResources
    Create JUnit ExternalResource types for both the CloudantClient and Database objects.
    This allows before and after methods to be applied at the test method or test class level
     by using the @Rule and @ClassRule annotations respectively, but keeps the setup and
    teardown code of these resources in a single place, allowing for easy enforcement of test
    best practice (such as using a unique Database name).
    
    BugId: 52580

---
+++ src/test/java/com/cloudant/tests/util/DatabaseResource.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2015 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.cloudant.tests.util;
+
+import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
+
+import org.junit.rules.ExternalResource;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.util.Locale;
+
+public class DatabaseResource extends ExternalResource {
+
+    private final CloudantClient client;
+    private String databaseName = Utils.generateUUID();
+    private Database database;
+
+    public DatabaseResource(CloudantClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        String testClassName = description.getClassName();
+        String testMethodName = description.getMethodName();
+        String uniqueSuffix = Utils.generateUUID();
+        databaseName = sanitizeDbName(testClassName + "-" + (testMethodName == null ? "" :
+                testMethodName + "-") + uniqueSuffix);
+        return super.apply(base, description);
+    }
+
+    /**
+     * The database must start with a letter and can only contain lowercase letters (a-z), digits
+     * (0-9) and the following characters _, $, (, ), +, -, and /.
+     *
+     * @param name to sanitize
+     * @return sanitized name
+     */
+    private static String sanitizeDbName(String name) {
+        //lowercase to remove any caps that will not be permitted
+        name = name.toLowerCase(Locale.ENGLISH);
+        //replace any characters that are not permitted with underscores
+        name = name.replaceAll("[^a-z0-9_\\$\\(\\)\\+\\-/]", "_");
+        return name;
+    }
+
+    @Override
+    public void before() {
+        database = client.database(databaseName, true);
+    }
+
+    @Override
+    public void after() {
+        client.deleteDB(databaseName);
+    }
+
+    public Database get() {
+        return this.database;
+    }
+
+}
