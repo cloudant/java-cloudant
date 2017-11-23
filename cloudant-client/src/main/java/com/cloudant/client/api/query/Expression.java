7b8bbad2e1ab8cc2d645f6b80ef63ce5921db6ba
Tom Blench <tom@blench.org>
2017-11-23 14:31:13 +0000

    Query selector builder (#377)
    * Query selector builder initial commit
    
    TODO:
    
    other stuff in the query body:
    - fields
    - sort
    - limit
    - skip
    
    documentation
    - javadoc
    - CHANGES
    - README
    
    * Fix `all` signature
    
    * Add support for fields, sort, limit, skip
    
    * findbugs
    
    * Add Type type; allow single element lists for in,nin,all
    
    * Copyright header
    
    * Narrow access
    
    * final specifiers

---
+++ cloudant-client/src/main/java/com/cloudant/client/api/query/Expression.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright © 2017 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.cloudant.client.api.query;
+
+import static com.cloudant.client.internal.query.Helpers.quote;
+
+// expression, such as "widget_count" "$eq" 5
+public class Expression implements OperationOrExpression {
+
+    private final String lhs;
+    private final String op;
+    private final Object[] rhs;
+    private boolean single;
+
+    private Expression(String lhs, String op, Object... rhs) {
+        this.lhs = lhs;
+        this.op = op;
+        this.rhs = rhs;
+    }
+
+    public static Expression lt(String lhs, Object rhs) {
+        return new Expression(lhs, "$lt", rhs);
+    }
+
+    public static Expression lte(String lhs, Object rhs) {
+        return new Expression(lhs, "$lte", rhs);
+    }
+
+    public static Expression eq(String lhs, Object rhs) {
+        return new Expression(lhs, "$eq", rhs);
+    }
+
+    public static Expression ne(String lhs, Object rhs) {
+        return new Expression(lhs, "$ne", rhs);
+    }
+
+    public static Expression gte(String lhs, Object rhs) {
+        return new Expression(lhs, "$gte", rhs);
+    }
+
+    public static Expression gt(String lhs, Object rhs) {
+        return new Expression(lhs, "$gt", rhs);
+    }
+
+    public static Expression exists(String lhs, boolean rhs) {
+        return new Expression(lhs, "$exists", rhs);
+    }
+
+    public static Expression type(String lhs, Type rhs) {
+        return new Expression(lhs, "$type", rhs.toString());
+    }
+
+    public static Expression in(String lhs, Object... rhs) {
+        Expression ex = new Expression(lhs, "$in", rhs);
+        if (rhs.length == 1) {
+            ex.single = true;
+        }
+        return ex;
+    }
+
+    public static Expression nin(String lhs, Object... rhs) {
+        Expression ex = new Expression(lhs, "$nin", rhs);
+        if (rhs.length == 1) {
+            ex.single = true;
+        }
+        return ex;
+    }
+
+    public static Expression size(String lhs, Long rhs) {
+        return new Expression(lhs, "$size", rhs);
+    }
+
+    public static Expression mod(String lhs, Long rhs) {
+        return new Expression(lhs, "$mod", rhs);
+    }
+
+    public static Expression regex(String lhs, String rhs) {
+        return new Expression(lhs, "$regex", rhs);
+    }
+
+    public static Expression all(String lhs, Object... rhs) {
+        Expression ex = new Expression(lhs, "$all", rhs);
+        if (rhs.length == 1) {
+            ex.single = true;
+        }
+        return ex;
+    }
+
+    @Override
+    public String toString() {
+        // lhs op rhs format
+        return String.format("\"%s\": {\"%s\": %s}", this.lhs, this.op, quote(this.rhs, this.single));
+    }
+
+}
---
+++ cloudant-client/src/main/java/com/cloudant/client/api/query/QueryBuilder.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2017 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.cloudant.client.api.query;
+
+import com.cloudant.client.internal.query.Helpers;
+
+import java.util.LinkedList;
+
+public class QueryBuilder {
+
+    private final OperationOrExpression selector;
+    private String[] fields;
+    private String[][] sort;
+    private Long limit;
+    private Long skip;
+
+    public QueryBuilder(OperationOrExpression selector) {
+        this.selector = selector;
+    }
+
+    public QueryBuilder fields(String... fields) {
+        this.fields = fields;
+        return this;
+    }
+
+    // TODO this is likely to take an array of FieldSorts or similar when merged with Rich's code
+    public QueryBuilder sort(String[]... sort) {
+        this.sort = sort;
+        return this;
+    }
+
+    public QueryBuilder limit(long limit) {
+        this.limit = limit;
+        return this;
+    }
+
+    public QueryBuilder skip(long skip) {
+        this.skip = skip;
+        return this;
+    }
+
+    public String build() {
+        String selectorString = selector.toString();
+        String fieldsString = this.fields == null ? null : Helpers.quote(this.fields);
+        String sortString = this.sort == null ? null : quoteSort(this.sort);
+        String limitString = this.limit == null ? null : Helpers.quote(this.limit);
+        String skipString = this.skip == null ? null : Helpers.quote(this.skip);
+        StringBuilder builder = new StringBuilder();
+        // build up components...
+        // selector
+        builder.append(String.format("\"selector\": {%s}", selectorString));
+        // fields
+        if (fieldsString != null) {
+            builder.append(String.format(", \"fields\": %s", fieldsString));
+        }
+        // sort
+        if (sortString != null) {
+            builder.append(String.format(", \"sort\": %s", sortString));
+        }
+        // limit
+        if (limitString != null) {
+            builder.append(String.format(", \"limit\": %s", limitString));
+        }
+        // skip
+        if (skipString != null) {
+            builder.append(String.format(", \"skip\": %s", skipString));
+        }
+        return String.format("{%s}", builder.toString());
+    }
+
+    // sorts are a bit more awkward and need a helper...
+    private static String quoteSort(String[][] sort) {
+        LinkedList<String> sorts = new LinkedList<String>();
+        for (String[] pair : sort) {
+            sorts.add(String.format("{%s}", Helpers.quoteNoSquare(pair)));
+        }
+        return sorts.toString();
+    }
+}
