7f4b8fd40caf0ee7788cde96edb95b12a45cc6e3
Tom Blench <tblench@uk.ibm.com>
2018-01-15 16:42:27 +0000

    Add missing Predicate Expressions

--- cloudant-client/src/main/java/com/cloudant/client/api/query/PredicateExpression.java
+++ cloudant-client/src/main/java/com/cloudant/client/api/query/PredicateExpression.java
@@ -30,6 +30,7 @@ public class PredicateExpression {
 
     private final String op;
     private final Object[] rhs;
+    private boolean single;
 
     private PredicateExpression(String op, Object... rhs) {
         this.op = op;
@@ -91,10 +92,100 @@ public class PredicateExpression {
         return new PredicateExpression("$gt", rhs);
     }
 
+    /**
+     * The field exists or not, regardless of its value
+     * @param rhs The argument (true or false)
+     * @return PredicateExpression: $exists rhs
+     */
+    public static PredicateExpression exists(boolean rhs) {
+        return new PredicateExpression("$exists", rhs);
+    }
+
+    /**
+     * The field has a given type
+     * @param rhs The type
+     * @return PredicateExpression: $type rhs
+     */
+    public static PredicateExpression type(Type rhs) {
+        return new PredicateExpression("$type", rhs.toString());
+    }
+
+    /**
+     * The document field must exist in the list provided
+     * @param rhs The argument - one or more values
+     * @return PredicateExpression: $in rhs
+     */
+    public static PredicateExpression in(Object... rhs) {
+        PredicateExpression ex = new PredicateExpression("$in", rhs);
+        if (rhs.length == 1) {
+            ex.single = true;
+        }
+        return ex;
+    }
+
+    /**
+     * The document field must not exist in the list provided
+     * @param rhs The argument - one or more values
+     * @return PredicateExpression: $nin rhs
+     */
+    public static PredicateExpression nin(Object... rhs) {
+        PredicateExpression ex = new PredicateExpression("$nin", rhs);
+        if (rhs.length == 1) {
+            ex.single = true;
+        }
+        return ex;
+    }
+
+    /**
+     * Special condition to match the length of an array field in a document. Non-array fields
+     * cannot match this condition
+     * @param rhs The length of the array
+     * @return PredicateExpression: $size rhs
+     */
+    public static PredicateExpression size(Long rhs) {
+        return new PredicateExpression("$size", rhs);
+    }
+
+    /**
+     * Divisor and Remainder are both positive or negative integers. Non-integer values result in a
+     * 404 status. Matches documents where the expression (field % Divisor == Remainder) is true,
+     * and only when the document field is an integer
+     * @param divisor The divisor argument of the mod operation
+     * @param remainder The remainder argument of the mod operation
+     * @return PredicateExpression: $mod divisor remainder
+     */
+    public static PredicateExpression mod(Long divisor, Long remainder) {
+        return new PredicateExpression("$mod", divisor, remainder);
+    }
+
+    /**
+     * A regular expression pattern to match against the document field. Matches only when the field
+     * is a string value and matches the supplied regular expression
+     * @param rhs The regular expression
+     * @return PredicateExpression: $regex rhs
+     */
+    public static PredicateExpression regex(String rhs) {
+        return new PredicateExpression("$regex", rhs);
+    }
+
+    /**
+     * Matches an array value if it contains all the elements of the argument array
+     * @param rhs The arguments
+     * @return PredicateExpression: $all rhs
+     */
+    public static PredicateExpression all(Object... rhs) {
+        PredicateExpression ex = new PredicateExpression( "$all", rhs);
+        if (rhs.length == 1) {
+            ex.single = true;
+        }
+        return ex;
+    }
+
+
     @Override
     public String toString() {
         // op rhs format
-        return String.format("\"%s\": %s", this.op, quote(this.rhs));
+        return String.format("\"%s\": %s", this.op, quote(this.rhs, this.single));
     }
 
 }
