f3249613632e1fa209a7a04c90169699bbc862fc
Tom Blench <tblench@uk.ibm.com>
2018-03-27 15:21:55 +0100

    Copyrights
    Note some files have had line endings converted to UNIX in the process

--- cloudant-client/src/test/java/com/cloudant/tests/ComplexKeySerializationTest.java
+++ cloudant-client/src/test/java/com/cloudant/tests/ComplexKeySerializationTest.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2015 IBM Corp. All rights reserved.
+ * Copyright © 2015, 2018 IBM Corp. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain key copy of the License at
+ * except in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
--- cloudant-client/src/test/java/com/cloudant/tests/ViewsTest.java
+++ cloudant-client/src/test/java/com/cloudant/tests/ViewsTest.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011 lightcouch.org
- * Copyright © 2015, 2016 IBM Corp. All rights reserved.
+ * Copyright © 2015, 2018 IBM Corp. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
