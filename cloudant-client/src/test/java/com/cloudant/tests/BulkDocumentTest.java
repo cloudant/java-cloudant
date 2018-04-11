37754b10e13d416f50998c3484acdbf4815c9efa
Tom Blench <tblench@uk.ibm.com>
2018-04-11 15:33:43 +0100

    Don't over-write reason for bulk()
    This allows users to retrieve reason string from failed validation
    functions, etc

--- cloudant-client/src/test/java/com/cloudant/tests/BulkDocumentTest.java
+++ cloudant-client/src/test/java/com/cloudant/tests/BulkDocumentTest.java
@@ -15,11 +15,16 @@
 package com.cloudant.tests;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isOneOf;
 
+import com.cloudant.client.api.model.DesignDocument;
 import com.cloudant.client.api.model.Response;
 import com.cloudant.test.main.RequiresDB;
 import com.cloudant.tests.base.TestWithDbPerClass;
+import com.cloudant.tests.base.TestWithDbPerTest;
 import com.google.gson.JsonObject;
 
 import org.junit.jupiter.api.Test;
@@ -28,7 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RequiresDB
-public class BulkDocumentTest extends TestWithDbPerClass {
+public class BulkDocumentTest extends TestWithDbPerTest {
 
     @Test
     public void bulkModifyDocs() {
@@ -39,6 +44,42 @@ public class BulkDocumentTest extends TestWithDbPerClass {
         List<Response> responses = db.bulk(newDocs);
 
         assertThat(responses.size(), is(2));
+    }
+
+    @Test
+    public void bulkRejectedByValidation() {
+        // create validation fn
+        DesignDocument ddoc = new DesignDocument();
+        ddoc.setId("_design/validationDesignDoc");
+        ddoc.setValidateDocUpdate("function (newDoc, oldDoc, userCtx, secObj) {\n" +
+                "    if (newDoc['title'] == 'forbidden') {\n" +
+                "        throw({forbidden: 'field cannot be forbidden' });\n" +
+                "    }\n" +
+                "}\n");
+        db.save(ddoc);
+        // create docs, one which will fail validation
+        List<Object> newDocs = new ArrayList<Object>();
+        newDocs.add(new Foo("doc1", "This is the document title"));
+        newDocs.add(new Foo("doc2", "forbidden"));
+        List<Response> responses = db.bulk(newDocs);
+        assertThat(responses, hasSize(2));
+        // Note that the CouchDB documents seem to be contradictory here, 201 always appears to be
+        // returned even if there are documents which fail validation.
+        // The documentation initially states
+        // "417 Expectation Failed – Occurs when at least one document was rejected by a validation
+        // function", but then later states
+        // "The return type from a bulk insertion will be 201 Created, with the content of the
+        // returned structure indicating specific success or otherwise messages on a per-document basis."
+        // Also note that the IBM Cloudant documentation makes no mention of a 417 status code in
+        // the documentation for _bulk_docs.
+        // We check for 201 or 202: "Note: If the write quorum cannot be met during an attempt to
+        // create a document, a 202 response is returned."
+        assertThat(responses.get(0).getStatusCode(), isOneOf(201, 202));
+        assertThat(responses.get(0).getReason(), is(nullValue()));
+        assertThat(responses.get(0).getError(), is(nullValue()));
+        assertThat(responses.get(1).getStatusCode(), isOneOf(201, 202));
+        assertThat(responses.get(1).getReason(), is("field cannot be forbidden"));
+        assertThat(responses.get(1).getError(), is("forbidden"));
     }
 
     @Test
