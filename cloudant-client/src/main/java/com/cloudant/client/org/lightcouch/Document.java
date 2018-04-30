3dd23705bc160f0746877db215b0af32899bc855
Tom Blench <tblench@uk.ibm.com>
2018-04-30 14:46:47 +0100

    Copyrights

--- cloudant-client/src/main/java/com/cloudant/client/org/lightcouch/Document.java
+++ cloudant-client/src/main/java/com/cloudant/client/org/lightcouch/Document.java
@@ -1,6 +1,6 @@
 /*
+ * Copyright © 2015, 2018 IBM Corp. All rights reserved.
  * Copyright (C) 2011 lightcouch.org
- * Copyright (c) 2015 IBM Corp. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
