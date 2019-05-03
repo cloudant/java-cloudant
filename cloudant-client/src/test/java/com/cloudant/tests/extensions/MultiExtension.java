e2966122ee59a9202f92426693b0184f5daa0d75
Rich Ellis <ricellis@users.noreply.github.com>
2019-05-03 16:30:03 +0100

    Fixed test extension ordering for after*

--- cloudant-client/src/test/java/com/cloudant/tests/extensions/MultiExtension.java
+++ cloudant-client/src/test/java/com/cloudant/tests/extensions/MultiExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 IBM Corp. All rights reserved.
+ * Copyright © 2018, 2019 IBM Corp. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -22,19 +22,29 @@ import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MultiExtension implements Extension, AfterAllCallback, AfterEachCallback,
         AfterTestExecutionCallback, BeforeAllCallback, BeforeEachCallback,
         BeforeTestExecutionCallback {
 
-    private final Extension[] extensions;
+    private final List<Extension> extensions;
+    private final List<Extension> reversedExtensions;
 
     public MultiExtension(Extension... extensions) {
-        this.extensions = extensions;
+        this.extensions = Arrays.asList(extensions);
+        // After tests we need to call extensions in the reverse order
+        this.reversedExtensions = new ArrayList<>(this.extensions.size());
+        this.reversedExtensions.addAll(this.extensions);
+        Collections.reverse(reversedExtensions);
     }
 
     @Override
     public void afterAll(ExtensionContext extensionContext) throws Exception {
-        for (Extension extension : extensions) {
+        for (Extension extension : reversedExtensions) {
             if (extension instanceof AfterAllCallback) {
                 ((AfterAllCallback) extension).afterAll(extensionContext);
             }
@@ -43,7 +53,7 @@ public class MultiExtension implements Extension, AfterAllCallback, AfterEachCal
 
     @Override
     public void afterEach(ExtensionContext extensionContext) throws Exception {
-        for (Extension extension : extensions) {
+        for (Extension extension : reversedExtensions) {
             if (extension instanceof AfterEachCallback) {
                 ((AfterEachCallback) extension).afterEach(extensionContext);
             }
@@ -52,7 +62,7 @@ public class MultiExtension implements Extension, AfterAllCallback, AfterEachCal
 
     @Override
     public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
-        for (Extension extension : extensions) {
+        for (Extension extension : reversedExtensions) {
             if (extension instanceof AfterTestExecutionCallback) {
                 ((AfterTestExecutionCallback) extension).afterTestExecution(extensionContext);
             }
